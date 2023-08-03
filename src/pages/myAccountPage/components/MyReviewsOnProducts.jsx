68dc363614f316e2736acbb3b23a919a276ddcdf
royalBoni <bonifaceemmanuel13@gmail.com>
2023-08-03 20:59:15 +0100

    update

--- src/pages/myAccountPage/components/MyReviewsOnProducts.jsx
+++ src/pages/myAccountPage/components/MyReviewsOnProducts.jsx
@@ -5,6 +5,7 @@ import { FaArrowLeft } from 'react-icons/fa';
 import { useGetTransactionByUserIdMutation } from '../../../features/transactionSlice.js/transaction';
 import { useFetchReviewByUserIdMutation } from '../../../features/dishReviews/dishReviewsSlice';
 import { useEffect } from 'react';
+import { selectAllDishes } from '../../../features/posts/postSlice';
 
 const MyReviewsOnProducts = ({toggleActiveNav}) => {
 
@@ -12,10 +13,17 @@ const MyReviewsOnProducts = ({toggleActiveNav}) => {
     const [fetchReviewByUserId, {data:myReviews}]= useFetchReviewByUserIdMutation()
     const myId = JSON.parse(localStorage.getItem('myUserId'))
 
+    const dishes = useSelector(selectAllDishes)
+
     const fetchCustomerTransaction = async()=>{
       await getTransactionByUserId({customerId:myId.id }).unwrap() 
       await fetchReviewByUserId({userId:myId.id }).unwrap()
   }
+
+  const returnProductImage = (id)=>{
+    const findImage=dishes.find((item)=>item._id===id)
+    return findImage.dish_image_url
+}
 
 
   useEffect(()=>{
@@ -58,7 +66,6 @@ const MyReviewsOnProducts = ({toggleActiveNav}) => {
     dishesArray.map((item)=>{
       isBoughtAndReviewed.map((rev)=>{
         if(item.dishId!==rev.dishId){
-          //nonReviewedDishes.push(item)
           if(nonReviewedDishes.find((checkItem)=>checkItem.dishId===item.dishId)){
             return null
           }
@@ -73,25 +80,33 @@ const MyReviewsOnProducts = ({toggleActiveNav}) => {
   }
 
   console.log(unReviewedPuchasedProducts())
-/*   console.log(myReviews) */
-  /* const reviewsOnBoughtProduct =()=>{
-    const isBoughtAndReviewed = []
-     myReviews?.data.map((review)=>{
-      unReviewedPuchasedProducts()?.map((product)=>{
-        if(review?.dishId===product?.dishId){
-          isBoughtAndReviewed.push(review)
-        }
-      })
-     })
-     return isBoughtAndReviewed
-  } */
 
 
     const pageWidth=useSelector((state)=>state.promptMessage.pageWidth);
   return (
     <div className='my-review-on-products'>
-        <div className='my-review-on-products-title'>{pageWidth<1000?<FaArrowLeft onClick={()=>toggleActiveNav(1)}/>:null} Orders</div>
-        MyReviewsOnProducts
+        <div className='my-review-on-products-title'>{pageWidth<1000?<FaArrowLeft onClick={()=>toggleActiveNav(1)}/>:null} Pending Reviews</div>
+        <div className="my-review-on-products-content">
+          {
+            unReviewedPuchasedProducts()?.map((item)=>{
+              return(
+                <div key={item._id} className="my-review-on-products-content-item">
+                  <div className="my-review-on-products-content-item-section">
+                    <div className="review-product-image">
+                      <img src={returnProductImage(item.dishId)} alt="" />
+                    </div>
+                    <div className="review-product-info">
+                      <div className="review-product-name">{item.dishName}</div>
+                      <div className="review-product-number">{`Order no: ${item._id}`}</div>
+                      <div className="review-product-delivery-date">Delivered on:</div>
+                    </div>
+                  </div>
+                  <div className="my-review-on-products-content-item-section">RATE THIS PRODUCT</div>
+                </div>
+              )
+            })
+          }
+        </div>
     </div>
   )
 }
